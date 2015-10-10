efae2397b65a174710a708cd9394b83c8e06deaa
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-10-10 10:23:41 -0400

    has_many :owner_tasks, class_name: 'TasksManagement::Task', foreign_key: :owner_id has_many :requester_tasks, class_name: 'TasksManagement::Task', foreign_key: :requester_id
    17 runs, 29 assertions, 0 failures, 0 errors, 0 skips

--- lib/tasks_management/has_tasks.rb
+++ lib/tasks_management/has_tasks.rb
@@ -1,4 +1,6 @@
 class ActiveRecord::Base 
 	def self.has_tasks
+		has_many :owner_tasks, class_name: 'TasksManagement::Task', foreign_key: :owner_id
+		has_many :requester_tasks, class_name: 'TasksManagement::Task', foreign_key: :requester_id
 	end
 end
--- test/models/tasks_management/task_test.rb
+++ test/models/tasks_management/task_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 module TasksManagement
   class TaskTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "the truth" do
+      assert true
+    end
   end
 end
