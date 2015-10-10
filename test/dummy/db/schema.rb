41b40024917d72df9196ce26bd218719189a9ae4
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-10-10 09:47:39 -0400

    rake db:migrate RAILS_ENV=test

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,20 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010133738) do
-
-  create_table "tasks_management_tasks", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.integer  "priority"
-    t.integer  "state"
-    t.integer  "parent_id"
-    t.string   "file"
-    t.integer  "owner_id"
-    t.integer  "requester_id"
-    t.date     "end_date"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
