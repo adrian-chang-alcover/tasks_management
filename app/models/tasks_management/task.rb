9b210cb0a6b09f90fba011794f975366e3c14ccf
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-10-10 11:43:32 -0400

    cattr_accessor :user TasksManagement::Task.user = self
    21 runs, 33 assertions, 0 failures, 0 errors, 0 skips

--- app/models/tasks_management/task.rb
+++ app/models/tasks_management/task.rb
@@ -1,4 +1,7 @@
 module TasksManagement
   class Task < ActiveRecord::Base
+  	cattr_accessor :user
+  	has_many :sub_tasks, class_name: "Task", foreign_key: "parent_id"
+  	belongs_to :parent, class_name: "Task"
   end
 end
