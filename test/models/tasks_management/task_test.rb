46005294f27529d0c5d85517fb91ee2049d7350d
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-10-10 10:47:25 -0400

    test "belongs_to_owner" test "belongs_to_requester"
    21 runs, 33 assertions, 0 failures, 0 errors, 0 skips

--- test/models/tasks_management/task_test.rb
+++ test/models/tasks_management/task_test.rb
@@ -5,5 +5,19 @@ module TasksManagement
     test "the truth" do
       assert true
     end
+
+    test "belongs_to_owner" do
+    	task = tasks_management_tasks(:one)
+    	user = users(:one)
+    	task.owner = user
+    	assert_equal user, task.owner
+	end
+
+	test "belongs_to_requester" do
+    	task = tasks_management_tasks(:one)
+    	user = users(:one)
+    	task.requester = user
+    	assert_equal user, task.requester
+	end
   end
 end
